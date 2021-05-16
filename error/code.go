165f5a9e387f24d0a415b9f7e9b96c058603e1f2
Yu YAGIHASHI <yagihash@mercari.com>
2021-05-16 17:17:41 +0900

    add rate_limited

--- error/code.go
+++ error/code.go
@@ -3,16 +3,15 @@ package error
 type Code string
 
 const (
-	InvalidJson         Code = "invalid_json"
-	InvalidRequestURL   Code = "invalid_request_url"
-	InvalidRequest      Code = "invalid_request"
-	ValidationError     Code = "validation_error"
-	Unauthorized        Code = "unauthorized"
-	RestrictedResource  Code = "restricted_resource"
-	ObjectNotFound      Code = "object_not_found"
-	ConflictError       Code = "conflict_error"
-	InternalServerError Code = "internal_server_error"
-	ServiceUnavailable  Code = "service_unavailable"
+	CodeInvalidJson         Code = "invalid_json"
+	CodeInvalidRequestURL   Code = "invalid_request_url"
+	CodeInvalidRequest      Code = "invalid_request"
+	CodeValidationError     Code = "validation_error"
+	CodeUnauthorized        Code = "unauthorized"
+	CodeRestrictedResource  Code = "restricted_resource"
+	CodeObjectNotFound      Code = "object_not_found"
+	CodeConflictError       Code = "conflict_error"
+	CodeRateLimited         Code = "rate_limited"
+	CodeInternalServerError Code = "internal_server_error"
+	CodeServiceUnavailable  Code = "service_unavailable"
 )
-
-var ()
